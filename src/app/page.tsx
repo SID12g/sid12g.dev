b39df744f4eff62b2d901d03b16654b4958eb09f
SID12g <sid12g@naver.com>
2023-12-14 12:21:02 +0900

    feat : ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image'
+import Profile from '../../public/profile_img.jpeg'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>sid12g.dev, comming soon!</div>
+    <div>
+      <div className={styles.wrap}><Image src={Profile} alt='profile' className={styles.img} /></div>
+      <div className={styles.wrap}><Link href='https://github.com/sid12g'><p>공사중</p></Link></div>
+    </div>
   )
 }
