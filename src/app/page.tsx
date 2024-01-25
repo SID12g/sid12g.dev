341baf8a72ef7d87365ef4985ce4321f06aa52dc
SID12g <sid12g@naver.com>
2024-01-26 01:31:15 +0900

    fix: del package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Links from "@/components/Links";
 import Portfolio from "@/components/Portfolio";
 import styles from "@/styles/page.module.css";
-import { GeistSans } from "geist/font/sans";
 
 export default function Home() {
   return (
     <div className={styles.wrap}>
       <div className={styles.row}></div>
-      <h1 className={`${styles.id} ${GeistSans.variable}`}>sid12g</h1>
+      <h1 className={styles.id}>sid12g</h1>
       <p className={styles.name}>조성민</p>
       <Portfolio />
       <Links />
