5f9c725a077bd8d8a734e0836588c1e026188ab1
SID12g <sid12g@naver.com>
2024-01-25 23:11:15 +0900

    del: all clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import Image from 'next/image'
-import Profile from '../../public/profile_img.jpeg'
-import styles from './page.module.css'
-import Link from 'next/link'
+import Image from "next/image";
+import Profile from "../../public/profile_img.jpeg";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div>
-      <div className={styles.wrap}><Image src={Profile} alt='profile' className={styles.img} /></div>
-      <div className={styles.wrap}><Link href='https://github.com/sid12g'><p>공사중</p></Link></div>
-    </div>
-  )
+  return <div></div>;
 }
