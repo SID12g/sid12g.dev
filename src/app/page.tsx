e82a2bcda9b6420427c5bcbe9112759bdf38c788
SID12g <sid12g@naver.com>
2024-05-18 23:11:01 +0900

    feat: notion db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import Profile from "@/components/profile";
 import Project from "@/components/project";
-import iphone from "@/../public/images/iphone.jpeg";
-import apple from "@/../public/images/apple.jpg";
 import Footer from "@/components/footer";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -11,62 +10,7 @@ export default function Home() {
       <div className="md:w-[calc(100%-350px)] md:ml-[350px] mt-[40px] md:mt-[60px] pl-[4%] pr-[4%]">
         <p className="mb-[20px] text-subtitle-color">projects</p>
         <div className="flex flex-wrap flex-col lg:flex-row justify-between">
-          <Project
-            href="?aaa"
-            image={iphone}
-            logo={apple}
-            name="iPhone 15 Pro"
-            description="새로운 혁신의 시작"
-          />
-          <Project
-            href="?aaa"
-            image={iphone}
-            logo={apple}
-            name="iPhone 15 Pro"
-            description="새로운 혁신의 시작"
-          />
-          <Project
-            href="?aaa"
-            image={iphone}
-            logo={apple}
-            name="iPhone 15 Pro"
-            description="새로운 혁신의 시작"
-          />
-          <Project
-            href="?aaa"
-            image={iphone}
-            logo={apple}
-            name="iPhone 15 Pro"
-            description="새로운 혁신의 시작"
-          />
-          <Project
-            href="?aaa"
-            image={iphone}
-            logo={apple}
-            name="iPhone 15 Pro"
-            description="새로운 혁신의 시작"
-          />
-          <Project
-            href="?aaa"
-            image={iphone}
-            logo={apple}
-            name="iPhone 15 Pro"
-            description="새로운 혁신의 시작"
-          />
-          <Project
-            href="?aaa"
-            image={iphone}
-            logo={apple}
-            name="iPhone 15 Pro"
-            description="새로운 혁신의 시작"
-          />
-          <Project
-            href="?aaa"
-            image={iphone}
-            logo={apple}
-            name="iPhone 15 Pro"
-            description="새로운 혁신의 시작"
-          />
+          <Project />
         </div>
         <Footer />
       </div>
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,35 +1,71 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense, use } from "react";
 
-export default function Project({
-  href,
-  image,
-  logo,
-  name,
-  description,
-}: {
-  href: string;
-  image: any;
-  logo: any;
-  name: string;
-  description: string;
-}) {
+interface Project {
+  id: string;
+  public_url: string;
+  icon: { file: { url: string } };
+  cover: { file: { url: string } };
+  properties: {
+    Name: { title: { plain_text: string }[] };
+    Description: { rich_text: { plain_text: string }[] };
+  };
+}
+
+async function fetchProjects() {
+  const response = await fetch(
+    `https://api.notion.com/v1/databases/${process.env.PROJECTS_DATABASE_ID}/query`,
+    {
+      method: "POST",
+      cache: "no-cache",
+      headers: {
+        Accept: "application/json",
+        "Notion-Version": "2022-02-22",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.NOTION_API_KEY}`,
+      },
+    }
+  );
+
+  return await response.json();
+}
+
+export default function Project() {
+  const data = use(fetchProjects());
   return (
-    <Link className="lg:w-[48%] mb-[40px]" href={href}>
-      <div className="rounded-[16px] border-[1px] border-border-color overflow-hidden">
-        <Image className="aspect-video" src={image} alt="project image" />
-        <div className="flex h-[92px] items-center">
-          <Image
-            className="w-[52px] h-[52px] rounded-[10px] border-[1px] border-border-color ml-[20px]"
-            src={logo}
-            alt="project logo image"
-          />
-          <div className="ml-[12px]">
-            <p className="text-[18px]">{name}</p>
-            <p className="text-[14px]">{description}</p>
+    <div className="flex flex-wrap flex-col lg:flex-row justify-between">
+      {data.results.map((project: Project, index: number) => (
+        <Link key={index} className="lg:w-[48%] mb-[40px]" href={"/"}>
+          <div className="rounded-[16px] border-[1px] border-border-color overflow-hidden">
+            <Image
+              className="aspect-video"
+              src={project.cover.file.url}
+              alt="project image"
+              width={1920}
+              height={1080}
+            />
+            <div className="flex h-[92px] items-center">
+              <Image
+                className="w-[52px] h-[52px] rounded-[10px] border-[1px] border-border-color ml-[20px]"
+                src={project.icon.file.url}
+                alt="project logo image"
+                width={256}
+                height={256}
+              />
+
+              <div className="ml-[12px]">
+                <p className="text-[18px]">
+                  {project.properties.Name?.title[0].plain_text}
+                </p>
+                <p className="text-[14px]">
+                  {project.properties.Description?.rich_text[0].plain_text}
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </Link>
+        </Link>
+      ))}
+    </div>
   );
 }
