f13791a8c7ee828c9dd177b0186ffb82b412a97b
SID12g <sid12g@naver.com>
2024-05-19 19:04:12 +0900

    fix: themeColor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#fafafa" },
+    { media: "(prefers-color-scheme: dark)", color: "#161616" },
+  ],
+};
 
 export const metadata: Metadata = {
   title: "sid12g • 조성민",
