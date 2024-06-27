65bc3a7ad56187de939cfcb95865e3095a0bb1c9
SID12g <sid12g@naver.com>
2024-06-27 14:37:13 +0900

    feat: projects detail meta info

--- src/app/projects/[name]/page.tsx
+++ src/app/projects/[name]/page.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
     <main className="w-full flex flex-col md:flex-row fixed">
       <Detail />
       <Profile sticky={false} />
-      <div className="md:w-[calc(100%-350px)] !md:ml-[350px] mt-[40px] md:mt-[60px] pl-[4%] pr-[4%]">
+      <div className="md:w-[calc(100%-350px)] mt-[40px] md:mt-[60px] pl-[4%] pr-[4%]">
         <p className="mb-[20px] text-subtitle-color">projects</p>
         <div className="flex flex-wrap flex-col lg:flex-row justify-between">
           <Project />
--- src/components/detail.tsx
+++ src/components/detail.tsx
@@ -1,27 +1,81 @@
+import Image from "next/image";
+import Link from "next/link";
+import X from "@/../public/images/x.svg";
+import advocate from "@/../public/images/advocate.png";
+import logo from "@/../public/images/logo.png";
+import team from "@/../public/images/team.svg";
+import date from "@/../public/images/date.svg";
+import source from "@/../public/images/source.svg";
+
 export default function Detail() {
   return (
-    <div
-      style={{
-        backgroundColor: "rgba( 0, 0, 0, 0.5 )",
-        position: "fixed",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 60,
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: "white",
-          width: "100%",
-          height: "100%",
-          borderRadius: 60,
-        }}
-      >
-        무ㅓㄴ데
+    <div className="overflow-scroll fixed inset-0 bg-black bg-opacity-50 flex justify-center pt-[60px] px-[28px] md:px-[60px]">
+      <div className="h-fit bg-background-color w-[100%] rounded-[60px] mb-[60px] pb-[44px]">
+        <div className="flex justify-end m-[44px]">
+          <Link href={"/"}>
+            <Image src={X} alt="X" />
+          </Link>
+        </div>
+        <article className="flex flex-col mx-[40px] md:mx-[100px]">
+          <Image
+            className="w-[100%] border border-border-color rounded-[20px]"
+            src={advocate}
+            alt="project-image"
+          />
+          <div className="flex w-[100%] px-[20px] py-[16px] border border-border-color bg-primary-color rounded-[16px] mt-[28px] mb-[20px]">
+            <Image
+              className="border border-border-color rounded-[12px] mr-[16px]"
+              src={logo}
+              alt="project-logo"
+              width={60}
+              height={60}
+            />
+            <div>
+              <p className="text-[24px] font-normal">advocate</p>
+              <p className="text-[12px] font-medium">
+                특수교육의 새로운 패러다임을 열다.
+              </p>
+            </div>
+          </div>
+          <div className="ml-[20px]">
+            <Property image={team} property="Team" content="DOTE" />
+            <Property image={date} property="Date" content="2024" />
+            <Source content="https://github.com" />
+          </div>
+          <hr className="mb-[20px]" />
+          <p>테스트</p>
+        </article>
       </div>
+    </div>
+  );
+}
+
+function Property({
+  image,
+  property,
+  content,
+}: {
+  image: any;
+  property: string;
+  content: string;
+}) {
+  return (
+    <div className="flex mb-[8px]">
+      <Image className="mr-[6px]" src={image} alt="property-icon" />
+      <p className="text-subtitle-color font-normal">{property}</p>
+      <p className="ml-[8px]">{content}</p>
+    </div>
+  );
+}
+
+function Source({ content }: { content: string }) {
+  return (
+    <div className="flex mb-[20px]">
+      <Image className="mr-[6px]" src={source} alt="property-icon" />
+      <p className="text-subtitle-color font-normal">Source</p>
+      <Link className="ml-[8px]" href={content}>
+        {content}
+      </Link>
     </div>
   );
 }
