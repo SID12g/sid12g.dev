463055cc73af0af132d606b2483ea51b8549b5be
SID12g <sid12g@naver.com>
2024-07-09 13:27:11 +0900

    feat: getProjects

---
+++ src/utils/getProjects.ts
@@ -0,0 +1,29 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const projectDir = "projects";
+const projectFiles = fs.readdirSync(path.join(process.cwd(), projectDir));
+
+const allProjects = projectFiles
+  .map((filename) => {
+    const fileContent = fs.readFileSync(
+      path.join(projectDir, filename),
+      "utf-8"
+    );
+    const { data: frontMatter } = matter(fileContent);
+    return {
+      meta: frontMatter,
+      slug: filename.replace(".mdx", ""),
+      content: fileContent,
+    };
+  })
+  .filter((post) => post.meta.tag);
+
+const Projects = allProjects.sort((a, b) => {
+  const dateA = a.meta.order.toLowerCase();
+  const dateB = b.meta.order.toLowerCase();
+  return dateB.localeCompare(dateA);
+});
+
+export { Projects };
