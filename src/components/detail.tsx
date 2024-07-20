e6b4b82a5ed016aeb0db97454b8c2921cc4609f4
SID12g <sid12g@naver.com>
2024-07-20 11:05:33 +0900

    fix: overflow scroll detail

--- src/components/detail.tsx
+++ src/components/detail.tsx
@@ -26,7 +26,7 @@ export default function Detail({
   content: string;
 }) {
   return (
-    <div className="overflow-scroll overflow-x-hidden fixed inset-0 bg-primary-color sm:bg-neutral-800/50 flex justify-center sm:pt-[60px] px-[0px] sm:px-[16px] md:px-[60px]">
+    <div className="overflow-y-scroll fixed inset-0 bg-primary-color sm:bg-neutral-800/50 flex justify-center sm:pt-[60px] px-[0px] sm:px-[16px] md:px-[60px]">
       <div className="h-fit bg-primary-color w-[100%] rounded-[32px] md:rounded-[32px] mb-[60px] pb-[44px] max-w-[800px]">
         <div className="flex justify-end m-[28px] mt-[40px] sm:m-[44px]">
           <Link href={"/"}>
