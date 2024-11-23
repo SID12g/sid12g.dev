bedd0868b25ee3c13dbf97c32397f6a6e616d022
SID12g <sid12g@naver.com>
2024-11-24 00:49:40 +0900

    fix: projects grid

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -4,14 +4,10 @@ import Link from "next/link";
 
 export default function Project() {
   return (
-    <div className="flex flex-wrap flex-col lg:flex-row justify-between text-font-color">
+    <div className="grid gap-6 grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 4xl:grid-cols-5 auto-fit">
       {sortProjects.map((project, index) => (
-        <Link
-          key={index}
-          className="lg:w-[48%] mb-[40px]"
-          href={"/projects/" + project.slug}
-        >
-          <div className="rounded-[16px] border-[1px] border-border-color overflow-hidden">
+        <Link key={index} className="mb-6" href={"/projects/" + project.slug}>
+          <div className="rounded-2xl border border-border-color overflow-hidden">
             <Image
               className="aspect-video"
               src={project.meta.image}
@@ -19,17 +15,17 @@ export default function Project() {
               width={1920}
               height={1080}
             />
-            <div className="flex h-[92px] items-center bg-primary-color">
+            <div className="flex h-24 items-center bg-primary-color">
               <Image
-                className="w-[52px] h-[52px] rounded-[10px] border-[1px] border-border-color ml-[20px]"
+                className="w-14 h-14 rounded-md border border-border-color ml-5"
                 src={project.meta.logo}
                 alt="project logo image"
                 width={256}
                 height={256}
               />
-              <div className="ml-[12px]">
-                <p className="text-[18px]">{project.meta.title}</p>
-                <p className="text-[14px]">{project.meta.description}</p>
+              <div className="ml-3">
+                <p className="text-lg">{project.meta.title}</p>
+                <p className="text-sm">{project.meta.description}</p>
               </div>
             </div>
           </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1124px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+      "4xl": "2560px",
+    },
     extend: {
       colors: {
         "font-color": "var(--font-color)",
