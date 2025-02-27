a6eca05bdc3d21d2c3711b9a5ad3e8e7a2b2a6a6
SID12g <sid12g@naver.com>
2025-02-27 23:22:20 +0900

    fix: Banner text inline

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,50 +2,42 @@
 
 import styled from "styled-components";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Banner() {
-  const [show, setShow] = useState(false);
   return (
     <div>
-      {show ? (
-        ""
-      ) : (
+      {
         <Wrapper>
-          현재 포트폴리오를 다시 개발하고 있습니다.
-          <Link href="https://v2.sid12g.dev">
-            <LinkText> (이전 버전으로 이동하기 ✨)</LinkText>
-          </Link>
-          <CloseIcon
-            onClick={() => {
-              setShow(true);
-            }}
-          >
-            ❎
-          </CloseIcon>
+          <Text>현재 포트폴리오를 다시 개발하고 있습니다.</Text>{" "}
+          <HyperLink
+            text="(이전 버전으로 이동하기 ✨)"
+            href="https://v2.sid12g.dev"
+          />
         </Wrapper>
-      )}
+      }
     </div>
   );
 }
 
+function HyperLink({ text, href }: { text: string; href: string }) {
+  return (
+    <Link href={href}>
+      <Text>{text}</Text>
+    </Link>
+  );
+}
+
 const Wrapper = styled.div`
-  font-size: 14px;
-  color: #ffffff;
   background-color: #000000;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  padding: 12px;
+  text-align: center;
+  & > * {
+    display: inline-block;
+  }
 `;
 
-const LinkText = styled.p`
-  margin-left: 8px;
+const Text = styled.p`
+  margin: 0;
   color: #ffffff;
-`;
-
-const CloseIcon = styled.span`
-  position: absolute;
-  right: 20px;
-  font-size: 18px;
-  cursor: pointer;
+  font-size: 14px;
 `;
