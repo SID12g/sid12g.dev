8bb8214571f30982bb08a1f584eb1d555d485aef
SID12g <sid12g@naver.com>
2025-03-02 00:05:37 +0900

    feat: Box component color props

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -3,10 +3,12 @@ import styled from "styled-components";
 const Box = styled.div<{
   width?: string;
   height?: string;
+  background?: string;
   radius?: string;
 }>`
   width: ${(props) => props.width};
   height: ${(props) => props.height};
+  background-color: ${(props) => props.background};
   border-radius: ${(props) => props.radius};
   overflow: hidden;
 `;
