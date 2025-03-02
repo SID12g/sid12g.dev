9e5f5c727aec1ca8eccead7586aaa2c9057a92c8
SID12g <sid12g@naver.com>
2025-03-02 20:46:04 +0900

    fix: Project component annotation

--- src/components/home/Project.tsx
+++ src/components/home/Project.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import styled from "styled-components";
-import Gap from "@/components/Gap";
-import Text from "@/components/Text";
-import Box from "@/components/Box";
-import ResponsiveGap from "@/components/ResponsiveGap";
+// import styled from "styled-components";
+// import Gap from "@/components/Gap";
+// import Text from "@/components/Text";
+// import Box from "@/components/Box";
+// import ResponsiveGap from "@/components/ResponsiveGap";
 import ContentFrame from "@/components/ContentFrame";
 
 export default function Project() {
@@ -25,10 +25,10 @@ export default function Project() {
   );
 }
 
-const BoxWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 200px);
-  gap: 32px;
-  right: 0;
-  position: relative;
-`;
+// const BoxWrapper = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(4, 200px);
+//   gap: 32px;
+//   right: 0;
+//   position: relative;
+// `;
