fd2550e95227d15d84542e61876f8a4ac03a95f9
sid12g <sead12g@gmail.com>
2026-02-24 10:59:34 +0900

    feat: Add Footer component with copyright notice.

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="mb-8">
+      <p className="mt-4 text-neutral-600 dark:text-neutral-300">
+        © {new Date().getFullYear()} sid12g All rights reserved.
+      </p>
+    </footer>
+  );
+}
