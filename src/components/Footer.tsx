0148737583369f0af0174c0f6407ad72cf9a61c3
sid12g <sead12g@gmail.com>
2026-02-24 10:32:41 +0900

    fix: line height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,39 +18,6 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-8">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/SID12g"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://blog.sid12g.dev"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">blog</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            href="mailto:i@sid12g.dev"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">email</p>
-          </a>
-        </li>
-      </ul>
       <p className="mt-4 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} sid12g All rights reserved.
       </p>
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,12 +1,12 @@
 export default function Intro() {
   return (
     <div className="flex flex-col">
-      <p className="text-xl font-medium leading-8 text-neutral-900 dark:text-neutral-100">
+      <p className="text-xl font-medium leading-normal text-neutral-900 dark:text-neutral-100">
         안녕하세요 😀
         <br />
         개발자 조성민입니다.
       </p>
-      <p className="mt-3 text-base leading-7 text-neutral-600 dark:text-neutral-400">
+      <p className="mt-3 text-base leading-normal text-neutral-600 dark:text-neutral-400">
         저는 서울시립대학교에 재학 중이며, 학업과 개발을 병행하며 다양한 분야에
         관심을 가지고 있습니다. 특히 개발, 경제, 금융에 큰 흥미를 두고 있으며,
         이에 대한 저의 생각을 블로그에서 공유하고 있습니다.
