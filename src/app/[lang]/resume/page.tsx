73be6016419f88b222dded08180d8f0ba195a7b3
sid12g <sead12g@gmail.com>
2026-07-06 16:40:44 +0900

    feat(resume): 언어별 이력서 링크 분기 처리
    - 언어 파라미터(en/ko)에 따라 다른 이력서 파일로 리다이렉트

--- src/app/[lang]/resume/page.tsx
+++ src/app/[lang]/resume/page.tsx
@@ -1,7 +1,17 @@
 import { redirect } from "next/navigation";
 
-export default function Resume() {
-  redirect(
-    "https://drive.google.com/file/d/1koB97tCYispWp8DfKJWuYFWIKkbRcm3i/view?usp=sharing",
-  );
+export default async function Resume({
+  params,
+}: {
+  params: Promise<{ lang: string }>;
+}) {
+  if ((await params).lang === "en") {
+    redirect(
+      "https://drive.google.com/file/d/1koB97tCYispWp8DfKJWuYFWIKkbRcm3i/view?usp=sharing",
+    );
+  } else {
+    redirect(
+      "https://drive.google.com/file/d/1h5CyQFuVjlpp5GRi-YgNBx0Q0RFtakdg/view?usp=sharing",
+    );
+  }
 }
