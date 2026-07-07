40ca029524f8d038ad4117872b25e63f7e188ca5
sid12g <sead12g@gmail.com>
2026-07-08 00:54:26 +0900

    fix(portfolio): 포트폴리오 리다이렉트 링크 최신화

--- src/app/[lang]/portfolio/page.tsx
+++ src/app/[lang]/portfolio/page.tsx
@@ -2,6 +2,6 @@ import { redirect } from "next/navigation";
 
 export default function Portfolio() {
   redirect(
-    "https://drive.google.com/file/d/1uo9v_BBKaeYewlLro596Yuo2R4_zws6n/view?usp=sharing",
+    "https://drive.google.com/file/d/1jfTKqfXtWm2CUNJ0pdVLn8x4gtp9TWGp/view?usp=sharing",
   );
 }
